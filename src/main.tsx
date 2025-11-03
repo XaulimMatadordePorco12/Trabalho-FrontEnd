a8ebbc0fb882d638c7e94a9dc1b7fcca4582ca8b
XaulimMatadordePorco12 <michael.silva3@estudante.ifms.edu.br>
2025-11-03 11:51:17 -0400

    Estevão - exclui rotas duplicadas

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter , Routes , Route} from 'react-router-dom'
-import Login from './componentes/login/login.tsx'
-import Erro from './componentes/erro/erro.tsx'
-import Carrinho from './componentes/carrinho/Carrinho.tsx'
+// import { BrowserRouter , Routes , Route} from 'react-router-dom'
+// import Login from './componentes/login/login.tsx'
+// import Erro from './componentes/erro/erro.tsx'
+// import Carrinho from './componentes/carrinho/Carrinho.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/error" element={<Erro/>} />
-        <Route path="/carrinho" element={<Carrinho/>} />
-
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </StrictMode>,
 )

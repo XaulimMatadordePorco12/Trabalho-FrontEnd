08399f482a7c194906d826e69cead2a2ddfb2f55
XaulimMatadordePorco12 <michael.silva3@estudante.ifms.edu.br>
2025-11-03 08:29:32 -0400

    adiciona a rota carrinho em main

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter , Routes , Route} from 'react-router-dom'
 import Login from './componentes/login/login.tsx'
 import Erro from './componentes/erro/erro.tsx'
+import Carrinho from './componentes/carrinho/Carrinho.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/error" element={<Erro/>} />
+        <Route path="/carrinho" element={<Carrinho/>} />
 
       </Routes>
     </BrowserRouter>

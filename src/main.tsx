580b782f003d5f359191394d1f4ef3bb0f1f4fe4
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-12-01 11:30:32 -0400

    Mariane - Renderização da página

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
 
 
-const stripe = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY); // sua chave pública
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/finalizar-compra" element={ <Elements stripe={stripe}><Componente que tenha o cartão/></Elements>} />
-			
-
-        
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </StrictMode>,
 )
